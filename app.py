0f52c200ced3e64d13c9ebc62a14a90612e80315
KavinduKL1 <kavindulakshanwijesinghe@gmail.com>
2025-12-27 11:39:50 -0500

    Fixed app.py syntax and enabled Host Network for ZAP

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-import sys
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, DevSecOps!"
+def hello():
+    return "Hello DevSecOps"
 
-if __name__ == '__main__':
-    # Console එකට Print එකක් දාමු App එක පටන් ගත්තා කියලා
-    print("Starting Flask Server...", file=sys.stderr)
-    # Debug අයින් කරලා, කෙලින්ම Run කරමු
-    app.run(host='0.0.0.0', port=5000, debug=False)
+# මේ කොටස හරියටම කෙලින් තියෙන්න ඕන (මුලටම)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000) # nosec
