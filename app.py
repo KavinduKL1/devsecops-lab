7f12dbacaaed9a7c3cd7a6fe4f9fcce330569abd
KavinduKL1 <kavindulakshanwijesinghe@gmail.com>
2025-12-27 11:07:19 -0500

    Ignored Bandit security warning for 0.0.0.0 bind

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def home():
 
 if __name__ == '__main__':
     # host='0.0.0.0' කියන්නේ ඕනම කෙනෙක්ට එන්න දොර අරින්න කියන එකයි
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000) # nosec
