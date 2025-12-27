4c77067bc76f4e42c7007e5b60c50179687d8a70
KavinduKL1 <kavindulakshanwijesinghe@gmail.com>
2025-12-27 11:13:47 -0500

    Simplified app.py to prevent startup crash

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello, DevSecOps!"
 
-if __name__ == '__main__':
-    # host='0.0.0.0' කියන්නේ ඕනම කෙනෙක්ට එන්න දොර අරින්න කියන එකයි
-    app.run(debug=True, host='0.0.0.0', port=5000) # nosec
+# IF කෑලි ඕන නෑ, කෙලින්ම Run කරන්න කියමු.
+# එතකොට Indentation වරදින්න විදියක් නෑ.
+app.run(debug=True, host='0.0.0.0', port=5000) # nosec
